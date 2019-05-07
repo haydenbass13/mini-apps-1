bd2b0e2766e4d81dda5e5cfcb77d2fe58df3355d
Hayden Bass <haydenbass@Haydens-MacBook-Pro.local>
2019-05-07 12:08:05 -0700

    server connected to client, dependencies installed

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,9 +1,22 @@
-`The server must flatten the JSON hierarchy, mapping each item/object in the JSON to a single line of CSV report (see included sample output), where the keys of the JSON objects will be the columns of the CSV report.
-You may assume the JSON data has a regular structure and hierarchy (see included sample file). In other words, all sibling records at a particular level of the hierarchy will have the same set of properties, but child objects might not contain the same properties. In all cases, every property you encounter must be present in the final CSV output.
-You may also assume that child records in the JSON will always be in a property called children.`
+// `The server must flatten the JSON hierarchy, mapping each item/object in the JSON to a single line of CSV report (see included sample output), where the keys of the JSON objects will be the columns of the CSV report.
+// You may assume the JSON data has a regular structure and hierarchy (see included sample file). In other words, all sibling records at a particular level of the hierarchy will have the same set of properties, but child objects might not contain the same properties. In all cases, every property you encounter must be present in the final CSV output.
+// You may also assume that child records in the JSON will always be in a property called children.`
 
 //all report generation logic is comes from here
 //dont use npm 
 //JQUERY NOT ALLOWED ON SERVER
 
+var express = require('express');
+var router = express.Router();
+var parser = require('body-parser');
+var app = express();
 
+app.use(express.static('client'));
+const port = 4568;
+
+app.listen(port, () => {
+  console.log(`Shortly is listening on ${port}`);
+});
+
+
+// app.post('/upload_json', (req, res) => { ... })
