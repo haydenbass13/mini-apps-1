e4cfa43355d17197e6279134218e43ee906cd6ef
Hayden Bass <haydenbass@Haydens-MacBook-Pro.local>
2019-05-08 13:07:26 -0700

    database created

---
+++ challenge_3/server/schema.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE POS;
+
+USE POS;
+
+CREATE TABLE users (
+  id INT NOT NULL,
+  firstName VARCHAR(100) NOT NULL,
+  lastName VARCHAR(100) NOT NULL,
+  email VARCHAR(200) UNIQUE,
+  pass VARCHAR(30),
+  salt VARCHAR(32),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE shippingInfo (
+  address VARCHAR(200) NOT NULL,
+  aptUnit VARCHAR(450),
+  city VARCHAR(100) NOT NULL,
+  st VARCHAR(2) NOT NULL,
+  zip VARCHAR(10) NOT NULL,
+  phone VARCHAR(10) NOT NULL,
+  userId INT NOT NULL,
+  FOREIGN KEY (userId) REFERENCES users(id)
+);
+
+CREATE TABLE billingInfo (
+  id INT NOT NULL,
+  credit INT(19) NOT NULL,
+  expy DATE,
+  cvv INT(3) NOT NULL,
+  billZip INT(5) NOT NULL,
+  userId INT NOT NULL,
+  FOREIGN KEY (userId) REFERENCES users(id)
+);
+
+
+
