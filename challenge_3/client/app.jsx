6d032a3d64c53354dbe1915a5e3235c2141b6196
Hayden Bass <haydenbass@Haydens-MacBook-Pro.local>
2019-05-08 17:13:42 -0700

    woohoooo rendering cyclically through the components

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,89 +1,103 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.handleClick = this.handleClick.bind(this);
     this.state = {
       component: HomePage,
     }
   }
   componentDidMount() {
-    console.log('mounted');
+    if(this.state.component === Confirmation) {
+      console.log('make request')
+    }
+    else {
+      console.log(this.state.component.name);
+    }
   }
+  handleClick(target) {
+    event.preventDefault();
+    let state = {component : target}
+    this.setState(state)
+
+  }
+
   render() {
-    let Component = this.state.component;
+    let Comp = this.state.component;
     return (
-    <div id='bigWrapper'>
-      <Component/>
-    </div>
-    )};
+      <div id='bigWrapper'>
+        <Comp click={this.handleClick}/>
+      </div>
+    )
+  };
 };
 
 //need to add click handlers for the checkout button that renders f1
 
-var HomePage = () => (
-  <button>Checkout</button>
+var HomePage = (props) => (
+  <button onClick={()=>props.click(F1)}>Checkout</button>
 );
 //f1
-var F1 = () => (
+var F1 = (props) => (
   //will be a form for account creation
-    //text areas:
-      //name first
-      //name last
-      //email --if time, verify working email
-      //password 
+  //text areas:
+  //name first
+  //name last
+  //email --if time, verify working email
+  //password 
   //should contain a next button that sends data to the db and renders f2 
-      <div className="form" id="f1">
-      <form >
-        <input type="text" placeholder="FirstName"/>
-        <input type="text" placeholder="LastName"/>
-        <input type="text" placeholder="Email Address"/>
-        <input type="password" placeholder="Password"/> 
-        <button type="submit" id="f1Button">Next</button>
-      </form>
-    </div> 
+  <div className="form" id="f1">
+    <form onSubmit={()=>props.click(F2)}>
+      <input type="text" placeholder="FirstName" />
+      <input type="text" placeholder="LastName" />
+      <input type="text" placeholder="Email Address" />
+      <input type="password" placeholder="Password" />
+      <button type="submit" id="f1Button" next="F2">Next</button>
+    </form>
+  </div>
 );
 
 // //f2
-var F2 = () => (
-//   //form for shipping information
-//     //text areas:
-//       //address 
-//         //line one  
-//         //line two (apt/unit number)
-//         //city  
-//         //state
-//         //zipcode
-//       //phone number
-//   //next button sends data to db and renders f3
-<div className="form" id="f2">
-  <form>
-    <input type="text" placeholder="Address"/>
-    <input type="text" placeholder="Apt/Unit"/>
-    <input type="text" placeholder="City"/>
-    <input type="text" placeholder="State"/>
-    <input type="text" placeholder="Zip"/>
-    <input type="text" placeholder="Phone Number"/>
-    <button type ="submit" id="f2Button">Next</button>
-  </form>
-</div>
-  
+var F2 = (props) => (
+  //   //form for shipping information
+  //     //text areas:
+  //       //address 
+  //         //line one  
+  //         //line two (apt/unit number)
+  //         //city  
+  //         //state
+  //         //zipcode
+  //       //phone number
+  //   //next button sends data to db and renders f3
+  <div className="form" id="f2" >
+    <form onSubmit={()=>props.click(F3)}>
+      <input type="text" placeholder="Address" />
+      <input type="text" placeholder="Apt/Unit" />
+      <input type="text" placeholder="City" />
+      <input type="text" placeholder="State" />
+      <input type="text" placeholder="Zip" />
+      <input type="text" placeholder="Phone Number" />
+      <button type="submit" id="f2Button">Next</button>
+    </form>
+  </div>
+
 );
 
 // //f3
 var F3 = (props) => (
-//   //form for payment information
-//   //text areas:
-//   //credit card #
-//   //expiry date
-//   //CVV
-//   //billing zip
-//   //next will send data to db and render confirmation page
-  <div class="form" id="f3">
-    <form>
-      <input type="text" placeholder="Card Number"/>
-      <input type="text" placeholder="Expiration Date"/>
-      <input type="text" placeholder="CVV"/>
-      <input type="text" placeholder="Billing Zip"/>
-      <button type="submit" id="f3Button">Next</button>
+  //   //form for payment information
+  //   //text areas:
+  //   //credit card #
+  //   //expiry date
+  //   //CVV
+  //   //billing zip
+  //   //next will send data to db and render confirmation page
+  <div className="form" id="f3">
+    <form onSubmit={()=>props.click(Confirmation)} >
+      <input type="text" name="cardNumber" placeholder="Card Number" />
+      <input type="text" placeholder="Expiration Date" />
+      <input type="text" placeholder="CVV" />
+      <input type="text" placeholder="Billing Zip" />
+      <button id="f3Button" value="Submit" >Next</button>
     </form>
   </div>
 );
@@ -92,10 +106,10 @@ var F3 = (props) => (
 //   //display all data from the db matching email and password 
 //   //have a purchase button at the bottom which returns to homepage
 var Confirmation = (props) => (
-  <div class="form" id="confirmation">
+  <div className="form" id="confirmation">
     <h1>Confirmation</h1>
 
   </div>
 )
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
